e9bd0146c203811c1cc5d2e1810de22afee75ed3
met94 <jopek94@o2.pl>
2016-06-07 23:16:33 +0200

    panele kierownika

---
+++ view/panel_kierownika.tpl.php
@@ -0,0 +1,158 @@
+        <!-- small navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top bootstrap-admin-navbar-sm" role="navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li>
+                                    <a href="index.html">Powrót do strony głównej<i class="glyphicon glyphicon-share-alt"></i></a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" role="button" class="dropdown-toggle" data-hover="dropdown"> <i class="glyphicon glyphicon-user"></i> Użytkownik <i class="caret"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Action</a></li>
+                      <!--                  <li role="presentation" class="divider"></li>  Linia dzieląca -->
+                                        <li><a href="index.html">Wyloguj się</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+<!-- PONIŻEJ PIERWSZY NAVBAR -->
+        <!-- main / large navbar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top bootstrap-admin-navbar bootstrap-admin-navbar-under-small" role="navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="index.php?action=panel_kierownika">Panel kierownika</a>
+                        </div>
+                  <!--      <div class="collapse navbar-collapse main-navbar-collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-hover="dropdown">Dropdown <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li role="presentation" class="dropdown-header">Dropdown header</li>
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li role="presentation" class="divider"></li>
+                                        <li role="presentation" class="dropdown-header">Dropdown header</li>
+                                        <li><a href="#">Separated link</a></li>
+                                        <li><a href="#">One more separated link</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div>
+                </div>
+            </div><!-- /.container -->
+        </nav>
+
+        <div class="container">
+            <!-- left, vertical navbar & content -->
+            <div class="row">
+                <!-- left, vertical navbar -->
+                <div class="col-md-2 bootstrap-admin-col-left">
+                    <ul class="nav navbar-collapse collapse bootstrap-admin-navbar-side">
+                        <li>
+                            <a href="index.php?action=panel_kierownika&typ=zamowienia_k"><span class="badge pull-right">731</span>Zamówienia</a>
+                        </li>
+                        <li>
+                            <a href="index.php?action=panel_kierownika&typ=faktura_k"><span class="badge pull-right">812</span>Faktury</a>
+                        </li>
+                        <li>
+                            <a href="index.php?action=panel_kierownika&typ=klienci_zalogowani_k"><span class="badge pull-right">27</span>Klienci</a>
+                        </li>
+                        <li>
+                            <a href="index.php?action=panel_kierownika&typ=pracownicy_k"><span class="badge pull-right">27</span>Pracownicy</a>
+                        </li>
+                         <li>
+                            <a href="index.php?action=panel_kierownika&typ=produkty_k"><span class="badge pull-right">2,221</span>Produkty</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="badge pull-right">2,221</span>Wiadomości</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="badge pull-right">11</span>Dostawcy</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <!-- content -->
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="page-header">
+                                <h1>Panel kierownika</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <div class="navbar navbar-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <footer role="contentinfo">
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/twitter-bootstrap-hover-dropdown.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap-admin-theme-change-size.js"></script>
+        <script type="text/javascript" src="vendors/jGrowl/jquery.jgrowl.js"></script>
+
+        <script type="text/javascript">
+            $(function() {
+                $('.tooltip').tooltip();
+                $('.tooltip-left').tooltip({placement: 'left'});
+                $('.tooltip-right').tooltip({placement: 'right'});
+                $('.tooltip-top').tooltip({placement: 'top'});
+                $('.tooltip-bottom').tooltip({placement: 'bottom'});
+
+                $('.popover-left').popover({placement: 'left', trigger: 'hover'});
+                $('.popover-right').popover({placement: 'right', trigger: 'hover'});
+                $('.popover-top').popover({placement: 'top', trigger: 'hover'});
+                $('.popover-bottom').popover({placement: 'bottom', trigger: 'hover'});
+
+                $('.notification').click(function() {
+                    var $id = $(this).attr('id');
+                    switch ($id) {
+                        case 'notification-sticky':
+                            $.jGrowl("Stick this!", {sticky: true});
+                            break;
+
+                        case 'notification-header':
+                            $.jGrowl("A message with a header", {header: 'Important'});
+                            break;
+
+                        default:
+                            $.jGrowl("Hello world!");
+                            break;
+                    }
+                });
+            });
+        </script>
